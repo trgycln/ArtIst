e136cf3c7c4896bfae1f85d1175b95f417c8bc39
trgycln <turgaycelen03@gmail.com>
2023-01-29 08:41:25 +0300

    swiper, countdown and general arrangements

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
-const acordion=document.querySelector(".acor-dion")
-const headerA=document.getElementById("headerA")
-const textA=document.getElementById("textA")
+// COUNTERDOWN
 
-headerA.addEventListener("click", ()=>{
-	textA.classList.toggle("hidden");
-})
+function newYear() {
+
+	let targetDay = new Date("09/10/2023").getTime()
+	const now = new Date().getTime()
+
+	const distance = targetDay - now
+
+	let second = 1000,
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
+
+	let remainingDay = Math.floor(distance / day)
+	let remainingHour = Math.floor((distance % day) / hour)
+	let remainingMinute = Math.floor((distance % hour) / minute)
+	let remainingSecond = Math.floor((distance % minute) / second)
+
+	document.getElementById("days").innerHTML = remainingDay;
+	document.getElementById("hours").innerHTML = remainingHour;
+	document.getElementById("minutes").innerHTML = remainingMinute;
+	document.getElementById("seconds").innerHTML = remainingSecond;
+
+}
+
+setInterval(() => {
+	newYear()
+}, 1000)
+
+
+// SSS ACORDİON 
+const acordionText = document.getElementById("acordionText");
+const openBtn = document.getElementById("openBtn");
+const closeBtn = document.getElementById("closeBtn")
+
+const myFunction2=()=>{
+	acordionText.classList.remove("disactive")
+	openBtn.classList.add("disactive")
+	closeBtn.classList.remove("disactive")
+
+}
+
+const myFunction=()=>{
+	acordionText.classList.add("disactive")
+	openBtn.classList.remove("disactive")
+	closeBtn.classList.add("disactive")
+}
+
+
+// SWİPER
+
+var swiper = new Swiper(".mySwiper", {
+	effect: "cards",
+	grabCursor: true,
+	cardsEffect: {
+		slidesOffsetBefore: 50, // This is 5px slide offset
+		slidesPerView: 1, // second slide partial show (In this question it was probably 3.5)
+		spaceBetween: 15,
+		rotate: false,
+		perSlideOffset: 25
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	}
+});
