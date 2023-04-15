92529e6a87a51440b71e8f25b95d84de613edb52
trgycln <turgaycelen03@gmail.com>
2023-04-15 11:55:47 +0300

    Mobile Menu arranged

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 // COUNTERDOWN
 
 function newYear() {
+  let targetDay = new Date("09/10/2023").getTime();
+  const now = new Date().getTime();
 
-	let targetDay = new Date("09/10/2023").getTime()
-	const now = new Date().getTime()
+  const distance = targetDay - now;
 
-	const distance = targetDay - now
+  let second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-	let second = 1000,
-		minute = second * 60,
-		hour = minute * 60,
-		day = hour * 24;
+  let remainingDay = Math.floor(distance / day);
+  let remainingHour = Math.floor((distance % day) / hour);
+  let remainingMinute = Math.floor((distance % hour) / minute);
+  let remainingSecond = Math.floor((distance % minute) / second);
 
-	let remainingDay = Math.floor(distance / day)
-	let remainingHour = Math.floor((distance % day) / hour)
-	let remainingMinute = Math.floor((distance % hour) / minute)
-	let remainingSecond = Math.floor((distance % minute) / second)
-
-	document.getElementById("days").innerHTML = remainingDay;
-	document.getElementById("hours").innerHTML = remainingHour;
-	document.getElementById("minutes").innerHTML = remainingMinute;
-	document.getElementById("seconds").innerHTML = remainingSecond;
+  document.getElementById("days").innerHTML = remainingDay;
+  document.getElementById("hours").innerHTML = remainingHour;
+  document.getElementById("minutes").innerHTML = remainingMinute;
+  document.getElementById("seconds").innerHTML = remainingSecond;
 }
 
 setInterval(() => {
-	newYear()
-}, 1000)
+  newYear();
+}, 1000);
 
-// SSS ACORDİON 
+// SSS ACORDİON
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active-acordion");
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
-	  this.children[1].style.display="none";
-	  this.children[2].style.display="block";
+      this.children[1].style.display = "none";
+      this.children[2].style.display = "block";
     } else {
       panel.style.display = "block";
-	  this.children[1].style.display="block";
-	  this.children[2].style.display="none";
+      this.children[1].style.display = "block";
+      this.children[2].style.display = "none";
     }
   });
 }
@@ -50,84 +49,108 @@ for (i = 0; i < acc.length; i++) {
 // SWİPER
 
 var swiper = new Swiper(".mySwiper", {
-	effect: "cards",
-	grabCursor: true,
-	cardsEffect: {
-		slidesOffsetBefore: 50, // This is 5px slide offset
-		slidesPerView: 1, // second slide partial show (In this question it was probably 3.5)
-		spaceBetween: 15,
-		rotate: false,
-		perSlideOffset: 25
-	},
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	}
+  effect: "cards",
+  grabCursor: true,
+  cardsEffect: {
+    slidesOffsetBefore: 50, // This is 5px slide offset
+    slidesPerView: 1, // second slide partial show (In this question it was probably 3.5)
+    spaceBetween: 15,
+    rotate: false,
+    perSlideOffset: 25,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
-
 
 // TOGGLE MENU
 
-const toggleMenu =()=>{
-	navbarContent.classList.toggle("active")
-}
+const yatayLogo = document.getElementById("yatayLogo")
+const closeBtn = document.getElementById("closeBtn")
 
+const toggleMenu = () => {
+  navbarContent.classList.toggle("active")
+  ;
+};
 
+closeBtn.addEventListener("click", ()=>{
+  navbarContent.classList.remove("active")
+})
 
 // ZOOM İMAGE
 
 function imageZoom(imgID, resultID) {
-	var img, lens, result, cx, cy;
-	img = document.getElementById(imgID);
-	result = document.getElementById(resultID);
-	/*create lens:*/
-	lens = document.createElement("DIV");
-	lens.setAttribute("class", "img-zoom-lens");
-	/*insert lens:*/
-	img.parentElement.insertBefore(lens, img);
-	/*calculate the ratio between result DIV and lens:*/
-	cx = result.offsetWidth / lens.offsetWidth;
-	cy = result.offsetHeight / lens.offsetHeight;
-	/*set background properties for the result DIV:*/
-	result.style.backgroundImage = "url('" + img.src + "')";
-	result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-	/*execute a function when someone moves the cursor over the image, or the lens:*/
-	lens.addEventListener("mousemove", moveLens);
-	img.addEventListener("mousemove", moveLens);
-	/*and also for touch screens:*/
-	lens.addEventListener("touchmove", moveLens);
-	img.addEventListener("touchmove", moveLens);
-	function moveLens(e) {
-	  var pos, x, y;
-	  /*prevent any other actions that may occur when moving over the image:*/
-	  e.preventDefault();
-	  /*get the cursor's x and y positions:*/
-	  pos = getCursorPos(e);
-	  /*calculate the position of the lens:*/
-	  x = pos.x - (lens.offsetWidth / 2);
-	  y = pos.y - (lens.offsetHeight / 2);
-	  /*prevent the lens from being positioned outside the image:*/
-	  if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-	  if (x < 0) {x = 0;}
-	  if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-	  if (y < 0) {y = 0;}
-	  /*set the position of the lens:*/
-	  lens.style.left = x + "px";
-	  lens.style.top = y + "px";
-	  /*display what the lens "sees":*/
-	  result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
-	}
-	function getCursorPos(e) {
-	  var a, x = 0, y = 0;
-	  e = e || window.event;
-	  /*get the x and y positions of the image:*/
-	  a = img.getBoundingClientRect();
-	  /*calculate the cursor's x and y coordinates, relative to the image:*/
-	  x = e.pageX - a.left;
-	  y = e.pageY - a.top;
-	  /*consider any page scrolling:*/
-	  x = x - window.pageXOffset;
-	  y = y - window.pageYOffset;
-	  return {x : x, y : y};
-	}
+  var img, lens, result, cx, cy;
+  img = document.getElementById(imgID);
+  result = document.getElementById(resultID);
+  /*create lens:*/
+  lens = document.createElement("DIV");
+  lens.setAttribute("class", "img-zoom-lens");
+  /*insert lens:*/
+  img.parentElement.insertBefore(lens, img);
+  /*calculate the ratio between result DIV and lens:*/
+  cx = result.offsetWidth / lens.offsetWidth;
+  cy = result.offsetHeight / lens.offsetHeight;
+  /*set background properties for the result DIV:*/
+  result.style.backgroundImage = "url('" + img.src + "')";
+  result.style.backgroundSize = img.width * cx + "px " + img.height * cy + "px";
+  /*execute a function when someone moves the cursor over the image, or the lens:*/
+  lens.addEventListener("mousemove", moveLens);
+  img.addEventListener("mousemove", moveLens);
+  /*and also for touch screens:*/
+  lens.addEventListener("touchmove", moveLens);
+  img.addEventListener("touchmove", moveLens);
+  function moveLens(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image:*/
+    e.preventDefault();
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    /*calculate the position of the lens:*/
+    x = pos.x - lens.offsetWidth / 2;
+    y = pos.y - lens.offsetHeight / 2;
+    /*prevent the lens from being positioned outside the image:*/
+    if (x > img.width - lens.offsetWidth) {
+      x = img.width - lens.offsetWidth;
+    }
+    if (x < 0) {
+      x = 0;
+    }
+    if (y > img.height - lens.offsetHeight) {
+      y = img.height - lens.offsetHeight;
+    }
+    if (y < 0) {
+      y = 0;
+    }
+    /*set the position of the lens:*/
+    lens.style.left = x + "px";
+    lens.style.top = y + "px";
+    /*display what the lens "sees":*/
+    result.style.backgroundPosition = "-" + x * cx + "px -" + y * cy + "px";
   }
+  function getCursorPos(e) {
+    var a,
+      x = 0,
+      y = 0;
+    e = e || window.event;
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return { x: x, y: y };
+  }
+}
+
+// Sidebar
+// function w3_open() {
+//   document.getElementById("mySidebar").style.display = "block";
+// }
+
+// function w3_close() {
+//   document.getElementById("mySidebar").style.display = "none";
+// }
